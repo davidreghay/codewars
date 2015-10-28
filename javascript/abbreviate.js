841aaa5253e75c1eeda3db69cb449401a8c85413
David Reghay <david.reghay@gmail.com>
2015-10-28 00:56:20 -0400

    Refactor abbreviate.js

--- javascript/abbreviate.js
+++ javascript/abbreviate.js
@@ -1,12 +1,7 @@
 function abbreviate(string) {
-  var words =  string.split(/[^A-Za-z]+/).map(function(el) {
-		if(el.length <= 3) return el;
-  	return el.replace(el.substr(1, el.length - 2), el.substr(1, el.length - 2).length.toString());
+  var match = string.match(/\w{4,}/gi);
+  if(match !== null) match.forEach(function(matcher) { 
+    string = string.replace(matcher.slice(1, matcher.length - 1), matcher.substr(1, matcher.length - 2).length) 
   });
-  var joiners = string.split(/[A-Za-z]+/);
-  var result = "";
-  for(var i = 0; i < words.length; i++) {
-  	result += words[i] + joiners[i + 1];
-  }
-  return result;
+  return string;
 }
